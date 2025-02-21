c5f98d32de190d34a0143d14557ff44d201c7d83
Dustin Brickwood <dustinbrickwood204@gmail.com>
2025-02-21 13:06:15 -0600

    test

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -18,7 +18,7 @@ const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      zksync: false,
+      zksync: true,
     },
     ZKsyncInMemory: {
       url: "http://127.0.0.1:8011",
